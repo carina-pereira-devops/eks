988d0255228c1857b217e6923a9d8d67b310e1f9
carina-pereira-devops <devops79@yahoo.com>
2025-06-12 20:24:14 -0300

    Nem sei mais o que eu mudei

--- iac_eks/main.tf
+++ iac_eks/main.tf
@@ -82,7 +82,7 @@ module "eks" {
 # OIDC Provider
 resource "aws_iam_openid_connect_provider" "eks-oidc" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks-certificate.certificates[0].sha1_fingerprint]
+  thumbprint_list = [data.tls_certificate.eks-certificate.certificates.sha1_fingerprint]
   url             = data.tls_certificate.eks-certificate.url
 }
 
