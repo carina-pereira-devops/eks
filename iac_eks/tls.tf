b29e861b62afb2c5175c2519e0321dbf39a0efa0
carina-pereira-devops <devops79@yahoo.com>
2025-06-12 20:36:49 -0300

    Nem sei mais o que eu mudei - parte 150

--- iac_eks/tls.tf
+++ iac_eks/tls.tf
@@ -1,5 +1,5 @@
 data "tls_certificate" "eks-certificate" {
-  url = cluster_name.identity.0.oidc.0.issuer
+  url = cluster_name.eks.identity.0.oidc.0.issuer
 }
 
 data "aws_iam_policy_document" "eks_oidc_assume_role_policy" {
