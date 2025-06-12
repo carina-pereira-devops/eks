26d7628f780e7240d8e91a230a2ba24ed5f1b725
carina-pereira-devops <devops79@yahoo.com>
2025-06-12 19:28:19 -0300

    Ajuste Nome Cluster

--- iac_eks/tls.tf
+++ iac_eks/tls.tf
@@ -1,5 +1,5 @@
 data "tls_certificate" "eks-certificate" {
-  url = aws_eks_cluster.eks[0].identity[0].oidc[0].issuer
+  url = local.cluster_name[0].identity[0].oidc[0].issuer
 }
 
 data "aws_iam_policy_document" "eks_oidc_assume_role_policy" {
